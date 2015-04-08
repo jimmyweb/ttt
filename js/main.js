cfd711beb0f6eda698466e3dd8bf816a80ca3034
Łukasz Leliński <lukasz.lelinski@xyo.net>
2015-04-08 16:40:25 +0200

    added navigation bar

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 	    counter.html(countdown(nowDate,targetDate, UNITS).toString());
 	  }, 1000);
 */
-	counter.FlipClock(targetDate.getTime() - nowDate.getTime(), {
+	/*counter.FlipClock(targetDate.getTime() - nowDate.getTime(), {
 		autostart: true,
 		clockFace: "DailyCounter",
 		countdown: true
@@ -18,7 +18,26 @@
 	console.log(targetDate.toString());
 	console.log(nowDate.toString());
 	console.log(targetDate.getTime() - nowDate.getTime());
-	console.log(targetDate.getDay(), nowDate.getDay());
+	console.log(targetDate.getDay(), nowDate.getDay());*/
+
+	var navHeight = $('.nav').height();
+
+	smoothScroll.init({
+	    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+	    easing: 'easeInOutCubic', // Easing pattern to use
+	    updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+	    offset: navHeight, // Integer. How far to offset the scrolling anchor location in pixels
+	    callbackBefore: function ( toggle, anchor ) {
+	    	console.log('callbackBefore');
+	    	console.log(toggle);
+	    	console.log(anchor);
+	    }, // Function to run before scrolling
+	    callbackAfter: function ( toggle, anchor ) {
+	    	console.log('callbackAfter');
+	    	console.log(toggle);
+	    	console.log(anchor);
+	    } // Function to run after scrolling
+	});
 
 	/*clock.setTime(targetDate.getTime() - nowDate.getTime());
 	clock.start();
